ce6227aaf7ca109563e854ade154612305fa74a0
TalifhaniTshifura <114491959+TalifhaniTshifura@users.noreply.github.com>
2024-01-13 12:27:24 +0200

    initial commit

---
+++ src/main/java/com/dailycodework/hillsideheavenhotel/HillSideHeavenHotelApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.hillsideheavenhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillSideHeavenHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HillSideHeavenHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dailycodework/hillsideheavenhotel/HillSideHeavenHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.hillsideheavenhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillSideHeavenHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
